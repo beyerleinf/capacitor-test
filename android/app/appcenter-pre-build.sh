9efff7d01fb5ee5d43f3f6196e0d16f99e868c0c
Fabian Beyerlein <dev@beyerleinf.de>
2022-11-24 14:26:55 +0100

    chore: Update build.gradle

---
+++ android/app/appcenter-pre-build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# fail if any command fails
+set -e
+# debug log
+# set -x
+
+# Required nodeJS version
+NODE_VERSION=16.18.1
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node16 "$NODE_VERSION"
+
+GitRev = $(git rev-parse --short HEAD)
+VersionSuffix = "test"
+Version = "1.$APPCENTER_BUILD_ID-$GitRev-$VersionSuffix"
+VersionCode = $APPCENTER_BUILD_ID
+
+printf "\n\
+VERSION=$Version\n\
+VERSION_CODE=$VersionCode\n\
+" > version.properties
